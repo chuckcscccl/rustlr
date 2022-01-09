858bf1862ec493858ac35492f18cdb06476f41ed
cscccl <chuck.c.liang@hofstra.edu>
2022-01-09 09:50:04 -0500

    tokenizer ready to move into rustlr

--- examples/newlexer/src/main.rs
+++ examples/newlexer/src/main.rs
@@ -2,19 +2,32 @@ use regex::Regex;
 use std::collections::{HashSet};
 use crate::RawToken::*;
 
-/// This struct is intended to replace Lextoken, and will not use owned string
+/// This struct is intended to replace Lextoken, and will not use owned string.
+/// It also includes the starting line and column positions of the token.
+/// Current this structure lives side-by side with Lextoken for compatibility.
 pub struct LexToken<'t,AT:Default>
 {
   pub sym: &'t str,
   pub value: AT,
+  pub line:usize,
+  pub column:usize,
 }
 impl<'t,AT:Default> LexToken<'t,AT>
 {
-  pub fn new(s:&'t str, v:AT) -> LexToken<'t,AT> { LexToken{sym:s, value:v} }
+  ///creates new lexical token with sym s, value v, line ln and column cl
+  pub fn new(s:&'t str, v:AT, ln:usize, cl:usize) -> LexToken<'t,AT>
+  { LexToken{sym:s, value:v, line:ln, column:cl} }
+  /// transfers lexical information (line/column) to new LexToken
+  pub fn transfer(&self, s:&'t str, v:AT) -> LexToken<'t,AT>
+  {  LexToken{sym:s, value:v, line:self.line, column:self.column} }
+  /// transfers lexical information from a (RawToken,line,column) triple
+  /// returned by [StrTokenizer::next_token] to a new LexToken with
+  /// sym s and value v.
+  pub fn from_raw(rt:(RawToken<'t>,usize,usize),s:&'t str,v:AT) -> LexToken<'t,AT>
+  { LexToken{sym:s, value:v, line:rt.1, column:rt.2} }
 }
 
 /// This trait is intended to replace Lexer, and won't use owned strings
-
 pub trait Tokenizer<AT:Default>
 {
   /// retrieves the next Lextoken, or None at end-of-stream. 
@@ -32,19 +45,38 @@ pub trait Tokenizer<AT:Default>
 
 
 #[derive(Debug)]
+/// structure produced by [StrTokenizer]
 pub enum RawToken<'t>
 {
+  /// an unsigned integer, though for convenience it is interpreted as
+  /// a signed number.  Negative numbers must be recognized by higher-level
+  /// parser.  Both decimal and hexadecimal numbers prefixed by 0x are
+  /// recognized.
   Num(i64),
-  Hex(u64),
+//  Hex(u64),
+  /// floating point number
   Float(f64),
+  /// String literal, allows for nested quotes
   Strlit(&'t str),
+  /// Alphanumeric sequence, staring with an alphabetical character or '_',
+  /// and followed by arbitrary numbers of alphabetical, numeric or _.
   Alphanum(&'t str),
+  /// non-alphanumeric character, either identified as doubles, singles, or
+  /// unrecognized sequences.
   Symbol(&'t str),
+  /// newline, returned optionally
   Newline,
-  Verbatim(&'t str),
+  /// number of consecutive whitespaces, returned optionally
   Whitespace(usize), // counts number of non-newline whitespaces
+  /// usually used to represent comments, if returned optionally
+  Verbatim(&'t str),
+  /// tokenizer error
+  LexError,
 }//RawToken
 
+//pub struct RawLexToken<'t>(RawToken<'t>,usize,usize);
+
+/// Generic str tokenizer that produces [RawToken]s.
 pub struct StrTokenizer<'t>
 {
    decuint:Regex,
@@ -53,19 +85,32 @@ pub struct StrTokenizer<'t>
    strlit:Regex,
    alphan:Regex,
    nonalph:Regex,
-   singletons:HashSet<char>,
-   symbols: Vec<&'t str>,
+   doubles:HashSet<&'t str>,   
+   singles:HashSet<char>,
+   other_syms: Vec<&'t str>,
    input: &'t str,
    position: usize,
+   /// flag to toggle whether whitespaces should be returned as Whitespace tokens,
+   /// default is false.
    pub keep_whitespace:bool,
+   /// flag to toggle whether newline characters ('\n') are returned as Newline
+   /// tokens. Default is false.  Note that if this flag is set to true then
+   /// newline characters are treated differently from other whitespaces.
+   /// For example, when parsing languages like Python, both keep_whitespace
+   /// and keep_newline should be set to true.  
    pub keep_newline:bool,
    line:usize,
    line_comment:&'t str,
-   pub keep_comments:bool,
+   ml_comment_start:&'t str,
+   ml_comment_end:&'t str,
+   /// flag to determine if comments are kept and returned as Verbatim tokens,
+   /// default is false.
+   pub keep_comment:bool,
    line_start:usize, // keep starting position of line, for column info
 }
 impl<'t> StrTokenizer<'t>
 {
+  /// creats a new tokenizer with defaults, *does not* set input.
   pub fn new() -> StrTokenizer<'t>
   {
     let decuint = Regex::new(r"^\d+").unwrap();
@@ -73,34 +118,72 @@ impl<'t> StrTokenizer<'t>
     let floatp = Regex::new(r"^\d*\x2E\d+").unwrap();
     let strlit = Regex::new(r"^\x22(?s)(.*)\x22").unwrap();
     let alphan = Regex::new(r"^[_a-zA-Z][_\da-zA-Z]*").unwrap();
-    let nonalph=Regex::new(r"^[!@#$%\^&*\?\-\+\*/\.,<>=~`';:\|\\]").unwrap();
-    let mut singletons = HashSet::with_capacity(16);
-    for c in ['(',')','[',']','{','}'] {singletons.insert(c);}
-    let mut symbols = Vec::with_capacity(32);
+    let nonalph=Regex::new(r"^[!@#$%\^&*\?\-\+\*/\.,<>=~`';:\|\\]+").unwrap();
+    let mut doubles = HashSet::with_capacity(16);    
+    let mut singles = HashSet::with_capacity(16);
+    for c in ['(',')','[',']','{','}'] {singles.insert(c);}
+    let mut other_syms = Vec::with_capacity(32);
     let input = "";
     let position = 0;
     let keep_whitespace=false;
     let keep_newline=false;
     let line = 1;
     let line_comment = "//";
-    let keep_comments=false;
+    let ml_comment_start="/*";
+    let ml_comment_end="*/";    
+    let keep_comment=false;
     let line_start=0;
-    StrTokenizer{decuint,hexnum,floatp,strlit,alphan,nonalph,singletons,symbols,input,position,keep_whitespace,keep_newline,line,line_comment,keep_comments,line_start}
+    StrTokenizer{decuint,hexnum,floatp,strlit,alphan,nonalph,doubles,singles,other_syms,input,position,keep_whitespace,keep_newline,line,line_comment,ml_comment_start,ml_comment_end,keep_comment,line_start}
   }// new
-  pub fn add_single(&mut self, c:char) { self.singletons.insert(c);}
-  pub fn add_symbol(&mut self, s:&'t str) {self.symbols.push(s); }
+  /// adds a symbol of exactly length two. If the length is not two the function
+  /// has no effect.  Note that these symbols override all other types except for
+  /// leading whitespaces and line comment markers, e.g. "//" will have precedence
+  /// over "/"
+  pub fn add_double(&mut self, s:&'t str)
+  {
+    if s.len()==2 { self.doubles.insert(s); }
+  }
+  /// add a single-character symbol.  The type of the symbol overrides other
+  /// types except for whitespaces and line comments.
+  pub fn add_single(&mut self, c:char) { self.singles.insert(c);}
+  /// add symbol of length greater than two. Symbols that are prefixes of
+  /// other symbols should be added after the longer symbols.
+  pub fn add_symbol(&mut self, s:&'t str) {
+    if s.len()>2 {self.other_syms.push(s); }
+  }
+  /// sets the input to be parsed, resets position information
   pub fn set_input(&mut self, inp:&'t str)
   {
     self.input=inp; self.position=0; self.line=1; self.line_start=0;
   }
+  /// sets the symbol that begins a single-line comment. The default is
+  /// "//".  If this is set to the empty string then no line-comments are
+  /// recognized.
   pub fn set_line_comment(&mut self,cm:&'t str) {
-    if cm.len()>0 {self.line_comment=cm;}
+    self.line_comment=cm;
   }
+  /// sets the symbols used to delineate multi-line comments using a
+  /// whitespace separated string such as "/* */".  These symbols are
+  /// also the default.  Set this to the empty string to disable
+  /// multi-line comments.
+  pub fn set_multiline_comments(&mut self,cm:&'t str)
+  {
+    if cm.len()==0 {
+      self.ml_comment_start=""; self.ml_comment_end=""; return;
+    }
+    let split:Vec<_> = cm.split_whitespace().collect();
+    if split.len()!=2 {return;}
+    self.ml_comment_start = split[0].trim();
+    self.ml_comment_end = split[1].trim();
+  }
+  /// the current line that the tokenizer is on
   pub fn line(&self)->usize {self.line}
+  /// the current column of the tokenizer
   pub fn column(&self)->usize {self.position-self.line_start+1}
 
-
-  /// returns next token, along with starting line and column numbers
+  /// returns next token, along with starting line and column numbers.
+  /// This function will return None at end of stream or LexError along
+  /// with a message printed to stderr if a tokenizer error occured.
   fn next_token(&mut self) -> Option<(RawToken<'t>,usize,usize)>
   {
     let mut pi = self.position; // should be set to start of token
@@ -114,7 +197,6 @@ impl<'t> StrTokenizer<'t>
     let mut nextchars = self.input[pi..].chars();
     let mut c = nextchars.next().unwrap();
     let mut i = pi;
-
     while c.is_whitespace() && i < self.input.len() 
     {
        if c=='\n' {
@@ -128,13 +210,62 @@ impl<'t> StrTokenizer<'t>
       self.position = i;
       return Some((Whitespace(i-pi),line0,self.column()-(i-pi)));}
     else {pi=i;}
-    c=self.input[pi..pi+1].chars().next().unwrap();
+    if pi>=self.input.len() {return None;}
 
-    // look for singleton:
-    if self.singletons.contains(&c) {
-      self.position=pi+1;
-      return Some((Symbol(&self.input[pi..pi+1]),self.line(),self.column()-1));
+    // look for line comment
+    let clen = self.line_comment.len();
+    if clen>0 && pi+clen<=self.input.len() && self.line_comment==&self.input[pi..pi+clen] {
+      if let Some(nlpos) = self.input[pi+clen..].find("\n") {
+        self.position = nlpos+pi+clen;
+        if self.keep_comment {
+          return Some((Verbatim(&self.input[pi..pi+clen+nlpos]),self.line,pi-self.line_start+1));
+        }
+        else {pi=self.position;}
+      } else { // no newline fould
+        self.position = self.input.len(); 
+        if self.keep_comment {return Some((Verbatim(&self.input[pi..]),self.line,pi-self.line_start+1));}
+        else {pi=self.position;}
+      }
+    }// line comment
+
+    // look for multi-line comment (similar to string literals)
+    let (cms,cme) = (self.ml_comment_start,self.ml_comment_end);
+    if cms.len()>0 && pi+cms.len()<=self.input.len() && &self.input[pi..pi+cms.len()] == cms {
+       if let Some(endpos) = self.input[pi+cms.len()..].find(cme) {
+         self.position = pi+cms.len()+endpos+cme.len();
+       } else {
+         self.position = self.input.len();
+         eprintln!("Tokenizer error: unclosed multi-line comment starting on line {}, column {}",line0,pi-self.line_start+1);
+         return Some((LexError,line0,pi-self.line_start+1));
+       }
+       // find newline chars
+       let mut ci = pi;
+       while let Some(nli) = self.input[ci..self.position].find('\n')
+       {
+          self.line+=1; ci += nli+1;  self.line_start=ci;
+          // Newline token is never returned if inside string literal
+       }
+       if self.keep_comment {
+         return Some((Verbatim(&self.input[pi..self.position]),line0,pi-lstart0+1));
+       }
+       else {pi=self.position;}
+    }//multi-line comments
+
+
+    // look for doubles
+    if pi+1<self.input.len() && self.doubles.contains(&self.input[pi..pi+2]) {
+      self.position = pi+2;
+      return Some((Symbol(&self.input[pi..pi+2]),self.line,self.column()-2));
     }
+
+    // look for singles:
+    //c=self.input[pi..pi+1].chars().next().unwrap();
+    if self.singles.contains(&c) {
+     // println!("ADDING SINGLE {}",c);
+      self.position=pi+1;
+      return Some((Symbol(&self.input[pi..pi+1]),self.line,self.column()-1));
+    }
+
     // look for string literal, keep track of newlines
     if let Some(mat) = self.strlit.find(&self.input[pi..]) {
        self.position = mat.end()+pi;
@@ -147,27 +278,11 @@ impl<'t> StrTokenizer<'t>
        }
        return Some((Strlit(&self.input[pi..self.position]),line0,pi-lstart0+1));
     }//string lits are matched first, so other's aren't part of strings
-    // look for line comment
-    
-    let clen = self.line_comment.len();
-    if clen>0 && pi+clen<=self.input.len() && self.line_comment==&self.input[pi..pi+clen] {
-      if let Some(nlpos) = self.input[pi+clen..].find("\n") {
-        self.position = nlpos+pi+clen;
-        if self.keep_comments {
-          return Some((Verbatim(&self.input[pi..pi+clen+nlpos]),self.line,pi-self.line_start+1));
-        }
-        else {pi=self.position;}
-      } else { // no newline fould
-        self.position = self.input.len(); 
-        if self.keep_comments {return Some((Verbatim(&self.input[pi..]),self.line,pi-self.line_start+1));}
-        else {pi=self.position;}
-      }
-    }// line comment
-    
-    // hex
+
+    // look for hex
     if let Some(mat) = self.hexnum.find(&self.input[pi..]) {
         self.position = mat.end()+pi;
-        return Some((Hex(u64::from_str_radix(&self.input[pi+2..self.position],16).unwrap()),self.line,pi+3-self.line_start));        
+        return Some((Num(i64::from_str_radix(&self.input[pi+2..self.position],16).unwrap()),self.line,pi+3-self.line_start));        
     }//hexnum
     // look for alphanum    
     if let Some(mat) = self.alphan.find(&self.input[pi..]) {
@@ -184,8 +299,20 @@ impl<'t> StrTokenizer<'t>
         self.position = mat.end()+pi;
         return Some((Float(self.input[pi..self.position].parse::<f64>().unwrap()),self.line,pi-self.line_start+1));
     }//floatp
-    // at this point, what remains must be a symbol, match longest symbols first
-    for sym in &self.symbols
+
+    //check for unclosed string
+    if pi<self.input.len() && &self.input[pi..pi+1]=="\"" {
+        eprintln!("Tokenizer error: unclosed string starting on line {}, column {}",line0,pi-self.line_start+1);
+        return Some((LexError,line0,pi-self.line_start+1));        
+    }//unclosed string
+      
+    // at this point, what remains must be a recognized, non-alphanumeric symbol
+    if let Some(mat) = self.nonalph.find(&self.input[pi..]) {
+        self.position = mat.end()+pi;
+        return Some((Symbol(&self.input[pi..self.position]),self.line,pi-self.line_start+1));	 
+    }
+    /*
+    for sym in &self.other_syms
     {
       let symlen = sym.len();
       if pi+symlen<=self.input.len() && &&self.input[pi..pi+symlen] == sym {
@@ -193,9 +320,23 @@ impl<'t> StrTokenizer<'t>
 	 return Some((Symbol(&self.input[pi..self.position]),self.line,pi-self.line_start+1));	 
       }
     }
+    */
+
+    // at this point, must be error
     self.position = self.input.len();
-    if pi<self.position {return Some((Verbatim(&self.input[pi..]),self.line,pi-self.line_start+1));}
-    else {return None;}
+    if pi<self.position {
+      if &self.input[pi..pi+1]=="\"" {
+         eprintln!("Tokenizer error: unclosed string starting on line {}, column {}",line0,pi-self.line_start+1);
+      }
+      else {
+       eprintln!("Tokenizer error: unrecognized symbols starting on line {}, column {}",line0,pi-self.line_start+1);
+      }
+      return Some((LexError,line0,pi-self.line_start+1));
+      //return Some((Verbatim(&self.input[pi..]),self.line,pi-self.line_start+1));
+    }
+    else {
+      return None;
+    }
   }//next  
 }//impl StrTokenizer
 
@@ -245,17 +386,23 @@ ere\""));
   st.f(8);
   //st.f("abc");
   let mut stk = StrTokenizer::new();
-  for x in ["==","=","+",";",",",":","!","*","/","-","<=","<"] {
-    stk.add_symbol(x);
+  for x in ['=','+',';',',','!','*','/','-','<'] {
+    stk.add_single(x);
   }
-  stk.keep_comments=true;
+  for x in ["==","<=","+="] { stk.add_double(x); }
+  stk.keep_comment=true;
   stk.keep_newline=true;
   stk.keep_whitespace=true;
   stk.set_input("{while (1==3.5-.7101*0x7E6) fork(x_y); //don't run
 printf(\"%d hello
  there!
 hello!\");
+::*- abcd @@&&
+
 x = x==      y;
+/* this is a test
+  of the emergency
+  broadcast system.  buzzz.. */
 return 0;
 }");
   let mut coln = stk.column();
@@ -289,17 +436,21 @@ impl STT for St
   fn f1(&self, i:&str) {println!("str, {} and {}",i,self.0);}
 }
 
+// testing an example
 impl<'t> Tokenizer<i64> for StrTokenizer<'t>
 {
    fn nextsym(&mut self) -> Option<LexToken<'t,i64>>
    {
-     match self.next() {
-       Some(Alphanum(s)) => Some(LexToken::new(s,0)),
-       Some(Num(x)) => Some(LexToken::new("num",x)),
-       Some(Strlit(s)) => Some(LexToken::new("strlit",2)),
-       Some(Symbol("@")) => Some(LexToken::new("ATSYM",3)),       
-       Some(Symbol(s)) => Some(LexToken::new(s,3)),
-       _ => Some(LexToken::new("EOF",0)),
+     let tokopt = self.next_token();
+     if let None=tokopt {return None;}
+     let tok = tokopt.unwrap();
+     match tok.0 {
+       Alphanum(s) => Some(LexToken::new(s,0,tok.1,tok.2)),
+       Num(x) => Some(LexToken::from_raw(tok,"num",x)),
+       Strlit(s) => Some(LexToken::from_raw(tok,"strlit",2)),
+       Symbol("@") => Some(LexToken::from_raw(tok,"ATSYM",3)),
+       Symbol(s) => Some(LexToken::from_raw(tok,s,3)),
+       _ => Some(LexToken::new("EOF",0,0,0)),
      }//match
    }
    fn current_line(&self) -> &str {self.input}
