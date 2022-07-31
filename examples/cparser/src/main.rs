26e42a6dbac984e5a33706ca37193212ee940f26
cscccl <chuck.c.liang@hofstra.edu>
2022-07-30 23:39:24 -0400

    c11 parser

--- examples/cparser/src/main.rs
+++ examples/cparser/src/main.rs
@@ -5,18 +5,22 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
 use rustlr::{LexSource,Tokenizer};
-
+/*
 mod cauto_ast;
 mod cautoparser;
 use cautoparser::*;
+*/
+mod c11_ast;
+mod c11parser;
+use c11parser::*;
 
 fn main()
 {
   let args:Vec<String> = std::env::args().collect(); // command-line args
-  let mut srcfile = "";
+  let mut srcfile = "test1.c";
   if args.len()>1 {srcfile = &args[1];}
   let source = LexSource::new(srcfile).unwrap();
-  let mut scanner2 = cautolexer::from_source(&source);
+  let mut scanner2 = c11lexer::from_source(&source);
   let mut parser2 = make_parser();
   let result2 = parse_with(&mut parser2, &mut scanner2);
   let absyntree2 = result2.unwrap_or_else(|x|{println!("Parsing Errors Encountered"); x});
