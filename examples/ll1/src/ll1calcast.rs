9353f1a2c3656ae55e4426ce2f830a434d6fbbda
Chuck Liang <chuckcscccl@gmail.com>
2023-06-06 23:21:11 -0700

    generic continuation

--- examples/ll1/src/ll1calcast.rs
+++ examples/ll1/src/ll1calcast.rs
@@ -6,6 +6,25 @@
 #![allow(dead_code)]
 
 
+pub struct Continuation<'t,T>( Box<dyn Fn(T) -> T + 't> );
+impl<'t,T> Default for Continuation<'t,T> {
+  fn default() -> Self {
+    Continuation (Box::new(|x|x))
+  }
+}// impl Default
+
+impl<'t,T> Continuation<'t,T> {
+  pub fn make(f:impl Fn(T) -> T + 't) -> Self {
+    Continuation( Box::new(f) )
+  }
+
+  pub fn apply(&self, n:T) -> T {
+    let Continuation(f) = self;
+    f(n)
+  }
+}//impl Continuation
+
+/*
 pub struct Continuation<'t>( Box<dyn Fn(i32) -> i32 + 't> );
 impl<'t> Default for Continuation<'t> {
   fn default() -> Self {
@@ -23,7 +42,7 @@ impl<'t> Continuation<'t> {
     f(n)
   }
 }//impl Continuation
-
+*/
 
 /*
 pub struct Continuation<'t>( dyn Fn(i32) -> i32 + 't );
