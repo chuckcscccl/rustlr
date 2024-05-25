020836016358302a8636f589464c7a376c042715
Chuck Liang <chuckcscccl@gmail.com>
2024-05-24 23:32:18 -0700

    java gen test

--- examples/java1/src/main.rs
+++ examples/java1/src/main.rs
@@ -1,15 +1,15 @@
 #![allow(unused_imports)]
 
-
+/*
 mod java14auto_ast;
 use java14auto_ast::*;
 mod java14autoparser;
-
-//mod java15_ast;
-//mod java15parser;
+*/
+mod java15_ast;
+mod java15parser;
 
 use rustlr::{LexSource,Tokenizer};
-/*
+
 fn main()
 {
   let args:Vec<String> = std::env::args().collect(); // command-line args
@@ -26,7 +26,7 @@ fn main()
   println!("\nParsing errors: {}",parser.error_occurred());
    
 }//main
-*/
+
 /*
 fn main()
 {
@@ -44,6 +44,7 @@ fn main()
 }//main
 */
 
+/*
 // for base_parser
 fn main()
 {
@@ -59,3 +60,23 @@ fn main()
   let ast = result.unwrap_or_else(|tree|{println!("Parsing errors encountered; results not guaranteed.."); tree});
    println!("\nAbstract Syntax: {:?}\n",&ast);
 }//main
+*/
+
+/*
+fn main()
+{
+  let args:Vec<String> = std::env::args().collect(); // command-line args
+  let mut srcfile = "";
+  if args.len()>1 {srcfile = &args[1];}
+  else {eprintln!("no source file given");}
+  let source = LexSource::new(srcfile).expect("Cannot open source file");
+  let mut scanner = java15parser::java15lexer::from_source(&source);
+  //let mut scanner = java15parser::java15lexer::from_str("...");  
+  let mut parser = java15parser::make_parser(scanner);
+  let result= java15parser::parse_with(&mut parser);
+  let ast = result.unwrap_or_else(|tree|{println!("Parsing errors encountered; results not guaranteed.."); tree});
+  println!("\nAbstract Syntax: {:?}\n",&ast);
+  println!("\nParsing errors: {}",parser.error_occurred());
+   
+}//main
+*/
