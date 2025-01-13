c0a8496ea386dc89ae273ae786d0c3b5f3e1fae7
chuckcscccl <chuck.c.liang@hofstra.edu>
2025-01-13 16:13:42 -0500

    wc example updated

--- examples/wildcard/src/main.rs
+++ examples/wildcard/src/main.rs
@@ -14,9 +14,9 @@ fn main()
   let args:Vec<String> = std::env::args().collect(); // command-line args
   let mut input = "a b j 44";
   if args.len()>1 {input = args[1].as_str();}
-   let mut scanner4 = wcparser::wclexer::from_str(input);
-   let mut parser4 = wcparser::make_parser();
-   let tree4= wcparser::parse_with(&mut parser4, &mut scanner4);
+   let scanner4 = wcparser::wclexer::from_str(input);
+   let mut parser4 = wcparser::make_parser(scanner4);
+   let tree4= wcparser::parse_with(&mut parser4);
    let result4 = tree4.unwrap_or_else(|x|{println!("Parsing errors encountered; results are partial.."); x});
    println!("\nABSYN: {:?}\n",&result4);
 }//main
